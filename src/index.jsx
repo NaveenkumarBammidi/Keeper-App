5b88d3e7887a0a9794d39aadfe77e30f891603ce
NaveenkumarBammidi <137760179+NaveenkumarBammidi@users.noreply.github.com>
2024-07-30 08:24:26 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,5 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+// Renders the main App component into the root element of the HTML.
+
